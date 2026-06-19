bc817d2104c859f1145724c25b2fa2ad496c963a
vsc-mindphin <vsc@mindphin.com>
2026-06-19 13:06:11 +0530

    [MOV][VSC]Moved sd_app_name module from soludoo19

---
+++ sd_app_name/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# soludoo Mader
+# Copyright (C) soludoo Mader(<https://www.soludoo.ch>).
+#
+###############################################################################
+
+{
+    'name': '(sd) App Name',
+    'version': '19.0.1.0',
+    'category': '', #provide category
+    'sequence': 1,
+    'summary': 'write here sort summery',
+    'description': "Add here description",
+    'website': 'https://www.soludoo.ch',
+    'author': 'Soludoo',
+    'depends': [''],
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': False,
+}
